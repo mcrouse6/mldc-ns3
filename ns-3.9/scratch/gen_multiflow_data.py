c70066013a069309e1daeef47297ea72bc389d65
Michael Crouse <croumb6@gmail.com>
2018-01-31 09:21:19 -0500

    cleaning the generation of flow and allocation scripts

--- ns-3.9/scratch/gen_multiflow_data.py
+++ ns-3.9/scratch/gen_multiflow_data.py
@@ -1,6 +1,18 @@
 import numpy as np
 import itertools
+import argparse
 
+
+# function to generate a flow between two points
+# writes entry to flow .dat file
+def create_flow_entry(idx, time_step, from_node, to_node):
+	data_per_timestep = 8193
+	data_rate = 1
+	# amount_data = np.random.randint(1e6, 1e8)#np.random.poisson(lam = internode_lambda)
+	amount_data = 100000000
+	flow_entry_str = '%d %d %d Tcp %d %dGbps' % (time_step, from_node, to_node, amount_data, data_rate)
+	flow_h.write(flow_entry_str)
+	flow_h.write('\n')
 
 #############################
 ######## PARAMETERS #########
@@ -14,8 +26,10 @@ num_TORs = 12
 time_steps = 1
 
 # minimum and maximum number of flows to genereate per timestep
-min_num_flows_per_timestep = 15
-max_num_flows_per_timestep = 16
+num_flows = 15 
+
+min_wireless_links = 2
+max_wireless_links = 5
 
 # interrack lambda for poisson sampling
 internode_lambda = 1.0
@@ -24,90 +38,82 @@ internode_lambda = 1.0
 num_wireless_allocations = 4
 
 # num allocs per flow file
-num_allocs_per_flow = 4
+num_allocs_per_flow = 15
 
 # output file name
-base_output_fn = 'new-flow'
-output_fn_flows = base_output_fn + '__flows.dat'
-output_fn_allocations = base_output_fn + '__alloc.dat'
+base_output_fn = 'flow-data-15-200-2'
 
-###########################
-###########################
-###########################
+num_files = 200
 
+for file_num in range(num_files):
+	output_fn_flows = '%s/flows__%d.dat' % (base_output_fn, file_num)
 
-# open .dat flow file
-flow_h = open(output_fn_flows, 'w')
-total_num_flows = 0
-total_num_allocs = 0
-
-# function to generate a flow between two points
-# writes entry to flow .dat file
-def create_flow_entry(idx, time_step, from_node, to_node):
-	data_per_timestep = 8193
-	data_rate = 1
-	# amount_data = np.random.randint(1e6, 1e8)#np.random.poisson(lam = internode_lambda)
-	amount_data = 100000000
-	flow_entry_str = '%d %d %d Tcp %d %dGbps' % (time_step, from_node, to_node, amount_data, data_rate)
-	flow_h.write(flow_entry_str)
-	flow_h.write('\n')
-
-	allocation_entry_str = '%d %d %d Tcp %d %dGbps' % (time_step, from_node, to_node, amount_data, data_rate)
+	###########################
+	###########################
+	###########################
 
 
-
-# list of idxs for TORs 
-# this aligns with the idxs used by NS-3
-TOR_idxs = np.arange(num_TORs)
-
-# create all possible pairs of flows
-all_pairs = list(itertools.permutations(TOR_idxs, 2)) 
+	# open .dat flow file
+	flow_h = open(output_fn_flows, 'w')
+	total_num_flows = 0
+	total_num_allocs = 0
 
 
+	# list of idxs for TORs 
+	# this aligns with the idxs used by NS-3
+	TOR_idxs = np.arange(num_TORs)
 
-# generate flows for each time step
-for time_step in range(time_steps):
-
-	# sample how many flows we will have at a given time step
-	num_flows = np.random.randint(min_num_flows_per_timestep, max_num_flows_per_timestep)
-
-	# select actual pairs of to and from nodes
-	flowidxs_to_use = np.random.choice(len(all_pairs), size=num_flows, replace=False)
-
-	# create actual entry
-	#for flowidx_to_use in range(len(flowidxs_to_use)):
-	for idx in range(len(flowidxs_to_use)):
-	# for flowidx_to_use in flowidxs_to_use:
-		# create_flow_entry(time_step, all_pairs[flowidx_to_use][0], all_pairs[flowidx_to_use][1])
-		create_flow_entry(idx, time_step, all_pairs[flowidxs_to_use[idx]][0], all_pairs[flowidxs_to_use[idx]][1])
-		total_num_flows += 1
-
-# close .dat file with flows
-flow_h.close()
-
-print "A total of %d flows have been created" % (total_num_flows)
-print "Flows saved in %s" % (output_fn_flows)
-
-for k in range(num_allocs_per_flow):
-	allocation_h = open(output_fn_allocations.split('.')[0] + '_' + str(k) + '.dat', 'w')
-
-	flow_h = open(output_fn_flows, 'r')
-	for i, line in enumerate(flow_h):
-
-		# generate wired/wireless allocations
-		wired_wireless_allocations = np.zeros(len(all_pairs))
-		wired_wireless_allocations[np.random.choice(num_flows, size=num_wireless_allocations, replace=False)] = 1
-		wired_wireless_allocations += 1
-
-		allocation_h.write(line.split('\n')[0] + ' %d' % (wired_wireless_allocations[i]))
-		allocation_h.write('\n')
-
-	allocation_h.close()
-
-	total_num_allocs += 1
+	# create all possible pairs of flows
+	all_pairs = list(itertools.permutations(TOR_idxs, 2)) 
 
 
-print "A total of %d allocs have been created for the flow log" % (total_num_allocs)
+	# generate flows for each time step
+	for time_step in range(time_steps):
+
+		# select actual pairs of to and from nodes
+		flowidxs_to_use = np.random.choice(len(all_pairs), size=num_flows, replace=False)
+
+		# create actual entry
+		for idx in range(len(flowidxs_to_use)):
+			create_flow_entry(idx, time_step, all_pairs[flowidxs_to_use[idx]][0], all_pairs[flowidxs_to_use[idx]][1])
+			total_num_flows += 1
+
+	# close .dat file with flows
+	flow_h.close()
+
+	print "A total of %d flows have been created" % (total_num_flows)
+	print "Flows saved in %s" % (output_fn_flows)
+
+	for k in range(num_allocs_per_flow):
+		# allocation_h = open(output_fn_allocations.split('.')[0] + '_' + str(k) + '.dat', 'w')
+		allocation_h = open("%s/alloc__%d_%d.dat" % (base_output_fn, file_num, k), 'w')
+
+		num_wireless = np.random.randint(min_wireless_links, max_wireless_links+1)
+		wireless_allocations = np.random.permutation(range(num_flows))[:num_wireless]
+
+		flow_h = open(output_fn_flows, 'r')
+		for i, line in enumerate(flow_h):
+
+			# generate wired/wireless allocations
+			# wired_wireless_allocations = np.zeros(len(all_pairs))
+			# wired_wireless_allocations[np.random.choice(num_flows, size=num_wireless_allocations, replace=False)] = 1
+			# wired_wireless_allocations += 1
+
+			if i in wireless_allocations:
+				link_t = 1
+			else:
+				link_t = 2
+
+			#allocation_h.write(line.split('\n')[0] + ' %d' % (wired_wireless_allocations[i]))
+			allocation_h.write(line.split('\n')[0] + ' %d' % (link_t))
+			allocation_h.write('\n')
+
+		allocation_h.close()
+
+		total_num_allocs += 1
+
+
+	print "A total of %d allocs have been created for the flow log" % (total_num_allocs)
 
 
 
